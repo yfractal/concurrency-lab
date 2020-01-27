b1c43b1cbad928251f9acc3bf57afaf20f76e49e
yfractal <yfractal@gmail.com>
2020-01-27 22:09:13 +0800

    feat: simplify segnment_index

--- lib/linear_hash.rb
+++ lib/linear_hash.rb
@@ -133,7 +133,7 @@ module LinearHash
       segnment_bit = Math.log @segnment_size, 2
 
       index = hash_val >> segnment_bit
-      if index < @next_segnment_index + 2.pow(@level)
+      if index < @table.size
         index
       else
         # drop first bit
